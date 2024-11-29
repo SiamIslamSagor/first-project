3dacb9085155a24094c7502d9518d856ee2adadf
SiamIslamSagor <msiamislam12345@gmail.com>
2024-11-30 00:17:07 +0600

    explore npm validator and specially joi validation for schema

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,21 +1,34 @@
-import { Request, Response } from 'express';
-import { StudentServices } from './student.service';
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+import studentValidationSchema from "./student.validation";
 
 const createStudent = async (req: Request, res: Response) => {
   try {
+    /* -------------------------------------------- */
+
+    /* -------------------------------------------- */
     const { student: studentData } = req.body;
+    const { error } = studentValidationSchema.validate(studentData);
 
     const result = await StudentServices.createStudentIntoDB(studentData);
 
+    if (error) {
+      res.status(500).json({
+        success: false,
+        message: "Something went wrong.",
+        error: error.details,
+      });
+    }
+
     res.status(200).json({
       success: true,
-      message: 'Student is created successfully',
+      message: "Student is created successfully",
       data: result,
     });
   } catch (error) {
     res.status(500).json({
       success: false,
-      message: 'Something went wrong.',
+      message: "Something went wrong.",
       error,
     });
   }
@@ -27,7 +40,7 @@ const getAllStudents = async (req: Request, res: Response) => {
 
     res.status(200).json({
       success: true,
-      message: 'Students are retrieved successfully',
+      message: "Students are retrieved successfully",
       data: result,
     });
   } catch (error) {
@@ -42,7 +55,7 @@ const getSingleStudent = async (req: Request, res: Response) => {
 
     res.status(200).json({
       success: true,
-      message: 'Student is retrieved successfully',
+      message: "Student is retrieved successfully",
       data: result,
     });
   } catch (error) {
--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -1,31 +1,35 @@
-import { model, Schema } from 'mongoose';
+import { model, Schema } from "mongoose";
 import {
   Guardian,
   LocalGuardian,
   Student,
   UserName,
-} from './student.interface';
+} from "./student.interface";
 
 const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
-    required: [true, 'First Name is required'],
+    required: [true, "First Name is required"],
     trim: true,
-    maxlength: [20, 'First name cannot be more the 20 characters '],
-    validate: {
+    maxlength: [20, "First name cannot be more the 20 characters "],
+    /* validate: {
       validator: function (value: string) {
         const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
 
         return firstNameStr === value;
       },
       message: '{VALUE} is not in capitalize format.',
-    },
+    }, */
   },
   middleName: { type: String, trim: true },
   lastName: {
     type: String,
-    required: [true, 'Last Name is required'],
+    required: [true, "Last Name is required"],
     trim: true,
+    /* validate: {
+      validator: (value: string) => validator.isAlpha(value),
+      message: '{VALUE} is not valid',
+    }, */
   },
 });
 
@@ -79,62 +83,66 @@ const studentSchema = new Schema<Student>({
   id: { type: String, required: true, unique: true },
   name: {
     type: userNameSchema,
-    required: [true, 'Name is required'],
+    required: [true, "Name is required"],
   },
   gender: {
     type: String,
     enum: {
-      values: ['male', 'female', 'other'],
-      message: '{VALUE} is not valid',
+      values: ["male", "female", "other"],
+      message: "{VALUE} is not valid",
     },
-    required: [true, 'Gender is required'],
+    required: [true, "Gender is required"],
   },
   dateOfBirth: { type: String },
   email: {
     type: String,
-    required: [true, 'Email is required'],
+    required: [true, "Email is required"],
     unique: true,
+    /* validate: {
+      validator: (value: string) => validator.isEmail(value),
+      message: '{VALUE} is a not valid email',
+    }, */
   },
   contactNo: {
     type: String,
-    required: [true, 'Contact Number is required'],
+    required: [true, "Contact Number is required"],
   },
   emergencyContactNo: {
     type: String,
-    required: [true, 'Emergency Contact Number is required'],
+    required: [true, "Emergency Contact Number is required"],
   },
   bloodGroup: {
     type: String,
     enum: {
-      values: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
-      message: 'Invalid blood group',
+      values: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      message: "Invalid blood group",
     },
   },
   presentAddress: {
     type: String,
-    required: [true, 'Present Address is required'],
+    required: [true, "Present Address is required"],
   },
   permanentAddress: {
     type: String,
-    required: [true, 'Permanent Address is required'],
+    required: [true, "Permanent Address is required"],
   },
   guardian: {
     type: guardianSchema,
-    required: [true, 'Guardian details are required'],
+    required: [true, "Guardian details are required"],
   },
   localGuardian: {
     type: localGuardianSchema,
-    required: [true, 'Local Guardian details are required'],
+    required: [true, "Local Guardian details are required"],
   },
   profileImg: { type: String },
   isActive: {
     type: String,
     enum: {
-      values: ['active', 'blocked'],
-      message: 'Status must be active or blocked',
+      values: ["active", "blocked"],
+      message: "Status must be active or blocked",
     },
-    default: 'active',
+    default: "active",
   },
 });
 
-export const StudentModel = model<Student>('Student', studentSchema);
+export const StudentModel = model<Student>("Student", studentSchema);
